fcaefcaf56d849c2940a4f084e1d3639d5f71e02
Agbleze <lagbleze@st.ug.edu.gh>
2022-07-16 11:35:31 +0200

    Create style.py

---
+++ style.py
@@ -0,0 +1,37 @@
+cardimg_style = {'width': '100%', 'height': '100%',
+              "opacity": 0.9, "objectFit": "cover"
+              }
+cardbody_style = {"margin": "2%"}
+
+cardstyling = {"maxWidth": 195,
+               "backgroundColor": "#e4ac23",
+            }
+
+card_style = {"width": "20rem", "height": "20rem"}
+
+card_icon = {
+    "color": "#0088BC",
+    "textAlign": "center",
+    "fontSize": "4em",
+    "margin": "auto"
+}
+
+homepage_icon_style = {
+                        "opacity": 0.9,
+                        "objectFit": "cover",
+                        "height": "100%",
+                        "width": "100%",
+                        }
+
+
+page_style = {"backgroundColor": "#FF8B00"}
+
+input_style={"color": '#0088BC'}
+
+button_style = {"backgroundColor": "#0088BC"}
+
+
+
+
+
+
